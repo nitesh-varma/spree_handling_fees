b6ac5cfccd94a1d49f8689df93fc79b67c239c99
Nitesh Varma <varmanitesh91@gmail.com>
2019-07-22 22:41:32 +0530

    Updated for rails 5

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,5 +1,8 @@
 Spree::AppConfiguration.class_eval do
-  Rails.application.config.spree.calculators.add_class('stock_locations')
+  Rails.application.config.spree.calculators.class_eval do
+    attr_accessor :stock_locations
+  end
+
   Rails.application.config.spree.calculators.stock_locations = [
     Spree::Calculator::Shipping::FlatPercentItemTotal,
     Spree::Calculator::Shipping::FlatRate,
